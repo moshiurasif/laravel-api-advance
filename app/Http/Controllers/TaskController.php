d6be02cd7516b1d49ace52206427dc72673cdcd2
Moshiur Asifm <asifmd2212@gmail.com>
2023-09-03 19:04:05 +0600

    factory setup

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,7 +13,7 @@ class TaskController extends Controller
 {
     public function index()
     {
-        return new TaskCollection(Task::all());
+        return new TaskCollection(Task::paginate());
     }
 
     public function show(Task $task)
